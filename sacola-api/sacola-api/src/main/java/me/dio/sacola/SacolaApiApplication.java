ef29c34857a6045127fb436a9c1d48ceb9a5fce4
Diógenes Beserra <diogenesbeserra@gmail.com>
2022-10-06 18:27:38 -0300

    create project desafio-iFood-POO.iml

--- sacola-api/sacola-api/src/main/java/me/dio/sacola/SacolaApiApplication.java
+++ sacola-api/sacola-api/src/main/java/me/dio/sacola/SacolaApiApplication.java
@@ -3,12 +3,18 @@ package me.dio.sacola;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
+import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
+
 @SpringBootApplication
 public class SacolaApiApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SacolaApiApplication.class, args);
+
+		
 	}
 
 }
